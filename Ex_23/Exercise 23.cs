79ec286442265d4510064e88fd57990301ae30ba
Vadim <lyzhinv@gmail.com>
2022-10-10 16:47:54 +0300

    Add Exercise 23

---
+++ Ex_23/Exercise 23.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число больше 0");
+double number = int.Parse(Console.ReadLine()!);
+int i = 1;
+
+if (number <= 0)
+{
+    Console.WriteLine("Введено не верное число. Пожалуйста, повторите попытку");
+}
+else
+{
+    while (i < number)
+    {
+        Console.Write($"{Math.Pow(i, 3)}, ");
+    i++;
+    }
+    Console.Write($"{Math.Pow(i, 3)}");
+}
